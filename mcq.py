6b79de0148ee2195932eb17a67ef798bbe01eb67
mohamedshafars19 <mohamedshafer.s.cyber29@princedrkvasudevan.com>
2026-02-10 10:15:39 +0530

    Add files via upload

---
+++ mcq.py
@@ -0,0 +1,206 @@
+score = 0
+
+print("WELCOME TO MCQ QUIZ")
+
+# Question 1
+print("\n1. What is 2 + 3?")
+print("1. 4")
+print("2. 5")
+print("3. 6")
+print("4. 7")
+ans = input("Enter your answer: ")
+if ans == "2":
+    score = score + 1
+
+# Question 2
+print("\n2. Which keyword is used to print output?")
+print("1. show")
+print("2. write")
+print("3. print")
+print("4. display")
+ans = input("Enter your answer: ")
+if ans == "3":
+    score = score + 1
+
+# Question 3
+print("\n3. What is the file extension of Python?")
+print("1. .pt")
+print("2. .py")
+print("3. .pyt")
+print("4. .python")
+ans = input("Enter your answer: ")
+if ans == "2":
+    score = score + 1
+
+# Question 4
+print("\n4. Which symbol is used for comments?")
+print("1. //")
+print("2. /*")
+print("3. #")
+print("4. --")
+ans = input("Enter your answer: ")
+if ans == "3":
+    score = score + 1
+
+# Question 5
+print("\n5. Which data type stores multiple values?")
+print("1. int")
+print("2. float")
+print("3. list")
+print("4. bool")
+ans = input("Enter your answer: ")
+if ans == "3":
+    score = score + 1
+
+# Question 6
+print("\n6. What is the output of 10 % 3?")
+print("1. 1")
+print("2. 3")
+print("3. 0")
+print("4. 10")
+ans = input("Enter your answer: ")
+if ans == "1":
+    score = score + 1
+
+# Question 7
+print("\n7. Which loop is used when count is known?")
+print("1. while")
+print("2. for")
+print("3. if")
+print("4. loop")
+ans = input("Enter your answer: ")
+if ans == "2":
+    score = score + 1
+
+# Question 8
+print("\n8. Which operator is used for power?")
+print("1. ^")
+print("2. **")
+print("3. //")
+print("4. %")
+ans = input("Enter your answer: ")
+if ans == "2":
+    score = score + 1
+
+# Question 9
+print("\n9. Which keyword is used for condition?")
+print("1. for")
+print("2. if")
+print("3. def")
+print("4. break")
+ans = input("Enter your answer: ")
+if ans == "2":
+    score = score + 1
+
+# Question 10
+print("\n10. Which function takes user input?")
+print("1. get()")
+print("2. read()")
+print("3. input()")
+print("4. scan()")
+ans = input("Enter your answer: ")
+if ans == "3":
+    score = score + 1
+
+# Question 11
+print("\n11. Which data type is immutable?")
+print("1. list")
+print("2. set")
+print("3. tuple")
+print("4. dict")
+ans = input("Enter your answer: ")
+if ans == "3":
+    score = score + 1
+
+# Question 12
+print("\n12. What does len() do?")
+print("1. Add")
+print("2. Delete")
+print("3. Length")
+print("4. Sort")
+ans = input("Enter your answer: ")
+if ans == "3":
+    score = score + 1
+
+# Question 13
+print("\n13. Which keyword stops a loop?")
+print("1. stop")
+print("2. exit")
+print("3. break")
+print("4. continue")
+ans = input("Enter your answer: ")
+if ans == "3":
+    score = score + 1
+
+# Question 14
+print("\n14. What is the index of first element?")
+print("1. 0")
+print("2. 1")
+print("3. -1")
+print("4. None")
+ans = input("Enter your answer: ")
+if ans == "1":
+    score = score + 1
+
+# Question 15
+print("\n15. Which stores key-value pairs?")
+print("1. list")
+print("2. tuple")
+print("3. set")
+print("4. dict")
+ans = input("Enter your answer: ")
+if ans == "4":
+    score = score + 1
+
+# Question 16
+print("\n16. What is the output of 5 // 2?")
+print("1. 2")
+print("2. 2.5")
+print("3. 3")
+print("4. 1")
+ans = input("Enter your answer: ")
+if ans == "1":
+    score = score + 1
+
+# Question 17
+print("\n17. Which keyword exits a function?")
+print("1. stop")
+print("2. break")
+print("3. exit")
+print("4. return")
+ans = input("Enter your answer: ")
+if ans == "4":
+    score = score + 1
+
+# Question 18
+print("\n18. Which function shows output?")
+print("1. print()")
+print("2. show()")
+print("3. display()")
+print("4. write()")
+ans = input("Enter your answer: ")
+if ans == "1":
+    score = score + 1
+
+# Question 19
+print("\n19. Which is a Boolean value?")
+print("1. Yes")
+print("2. No")
+print("3. True")
+print("4. 1")
+ans = input("Enter your answer: ")
+if ans == "3":
+    score = score + 1
+
+# Question 20
+print("\n20. Which loop runs while condition is true?")
+print("1. for")
+print("2. if")
+print("3. while")
+print("4. do")
+ans = input("Enter your answer: ")
+if ans == "3":
+    score = score + 1
+
+print("\nQUIZ FINISHED")
+print("Your Score:", score, "/ 20")
